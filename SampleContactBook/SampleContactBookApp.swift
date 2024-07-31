7d106a261e7dbc5eda8a8563f6320afea478ce98
Viswanadh <viswanadh.n@krify.com>
2024-07-31 10:31:04 +0530

    Initial Commit

---
+++ SampleContactBook/SampleContactBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleContactBookApp.swift
+//  SampleContactBook
+//
+//  Created by KSMACMINI-016 on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleContactBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
